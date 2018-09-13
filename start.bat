47273fb0f73f0d199fb1362f202b2424b31c558b
yuleMeng <34034405+yuleMeng@users.noreply.github.com>
2018-09-13 16:15:07 +0900

    Create start.bat
    添加一键执行

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off  
+D:\test\mechine_learning\venv\Scripts\pip install -r requirements.txt
+cd D:\workspace\pyMarkdown-master\app
+D:\test\mechine_learning\venv\Scripts\python main.py
+ECHO pyMd2Doc is execute successfully!!!
+pause
